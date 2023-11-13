48c0d231e31a37aec545fce717c75ab0207e7aca
myquyen_nguyen <quyen.ngthmy@gmail.com>
2023-11-13 22:45:49 +0700

    first commit

---
+++ source/src/js/index.js
@@ -0,0 +1,15 @@
+import "../scss/index.scss";
+import "./reveal";
+import "./inputNumber";
+import "./dropdown";
+import "./menu";
+import "./scroll-to-top";
+import "./dropdown-filter";
+import "./checkAll";
+import "./modal-variant";
+
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
